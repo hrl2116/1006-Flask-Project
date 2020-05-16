9608bcf64dfed31a8a16129603a83b5b910df44e
hrl2116 <hrl2116@columbia.edu>
2020-05-15 20:49:54 -0400

    added docstrings and other comments to the files

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-# -*- coding: utf-8 -*-
+################################
+# Final Exam
+# ENGI 1006
+# Name: Hannah Luo
+# UNI: hrl2116
+# File app.py contains the routes of the server
+################################
 """
 Created on Tue Apr 21 14:57:17 2020
 
@@ -12,15 +18,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 #static route
+'''for the homepage'''
 @app.route("/")
 def hello():
     #return "Hello World!"
     return render_template("index.html")
 
+'''for the assignments page'''
 @app.route("/assignments")
 def test123():
     return render_template("1006.html")
 
+'''for the classes page'''
 @app.route("/classes")
 def blah():
     return render_template("classes.html")
